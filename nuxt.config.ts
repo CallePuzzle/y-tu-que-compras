09a0b9f2a19934b94d16daf53cc0879ef022b590
César M. Cristóbal <cesar@callepuzzle.com>
2022-06-22 22:22:08 +0200

    add materialize

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+          meta: [
+            // <meta name="viewport" content="width=device-width, initial-scale=1">
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+          ],
+          script: [
+            // <script src="https://myawesome-lib.js"></script>
+            { src: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js' }
+          ],
+          link: [
+            // <link rel="stylesheet" href="https://myawesome-lib.css">
+            { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css' }
+          ],
+        }
+      }
 })
