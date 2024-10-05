8088e27c2f20d8a9ba6a61d9a01ad5b63d01afcf
César M. Cristóbal <cesar@callepuzzle.com>
2024-10-05 18:30:06 +0200

    detalle de home

--- src/routes/home/[slug]/+page.svelte
+++ src/routes/home/[slug]/+page.svelte
@@ -1,2 +1,26 @@
 <script lang="ts">
+	import type { PageData } from './$types';
+
+	let {
+		data
+	}: {
+		data: PageData;
+	} = $props();
+
+	const home = data.home;
 </script>
+
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content flex-col lg:flex-row">
+		<img
+			src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+			class="max-w-sm rounded-lg shadow-2xl"
+		/>
+		<div>
+			<h1 class="text-5xl font-bold">{home.name}</h1>
+			<p class="py-6">
+				{home.description}
+			</p>
+		</div>
+	</div>
+</div>
