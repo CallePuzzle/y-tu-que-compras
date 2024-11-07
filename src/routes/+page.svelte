802d3fc120351a7188e75f3e120dd38f0ee0af91
César M. Cristóbal <cesar@callepuzzle.com>
2024-11-07 16:21:28 +0100

    pintamos el icono de la casa y arreglamos estilos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { t } from '$lib/translations';
 	import AddEditModal from '$lib/components/forms/AddEditModal.svelte';
 	import { HomeSchema } from '$lib/schemas';
 	import { Routes } from '$lib/routes';
+	import { Icon } from 'svelte-icons-pack';
 
 	import type { PageData } from './$types';
+	import type { IconType } from 'svelte-icons-pack';
+	import type { Home } from '@prisma/client';
 
 	let {
 		data
 	}: {
 		data: PageData;
 	} = $props();
+
+	let iconComponent: IconType[] = $state([]);
+
+	async function loadIcons(houses: Home[]) {
+		for (const house of houses) {
+			try {
+				const icon = (await import(`svelte-icons-pack/ai`))[house.icon] as IconType;
+				iconComponent.push(icon);
+			} catch (error) {
+				console.error('Error loading icon:', error);
+			}
+		}
+	}
+
+	onMount(async () => {
+		loadIcons(data.houses);
+	});
 
 	const superform = data.form;
 </script>
@@ -22,12 +43,19 @@
 			<p class="py-6">
 				{$t('index.selectHome')}
 			</p>
-			{#each data.houses as home}
+			{#each data.houses as home, index}
 				<div class="card bg-base-100 shadow-xl my-4">
 					<a href={Routes.home_index.generateUrl({ id: home.id })}>
-						<div class="card-body">
-							<h2 class="card-title">{home.name}</h2>
-							<p>{home.description}</p>
+						<div class="card-body flex flex-row">
+							<div class="basis-1/2">
+								{#if iconComponent[index]}
+									<Icon src={iconComponent[index]} size="2.5em" />
+								{/if}
+							</div>
+							<div class="basis-1/2">
+								<h2 class="card-title">{home.name}</h2>
+								<p class="flex">{home.description}</p>
+							</div>
 						</div>
 					</a>
 				</div>
