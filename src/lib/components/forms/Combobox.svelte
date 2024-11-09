9b259f59ae5796c2ca7cbabbdc175c5c95bb6be1
César M. Cristóbal <cesar@callepuzzle.com>
2024-11-09 13:49:35 +0100

    estilos al combobox

--- src/lib/components/forms/Combobox.svelte
+++ src/lib/components/forms/Combobox.svelte
@@ -10,6 +10,8 @@
 	import { createCombobox, melt, type ComboboxOptionProps } from '@melt-ui/svelte';
 	import { fly } from 'svelte/transition';
 	import { Control, Field, FieldErrors, Label } from 'formsnap';
+	import { Icon } from 'svelte-icons-pack';
+	import { AiOutlineUp, AiOutlineDown } from 'svelte-icons-pack/ai';
 	import { type SuperForm, formFieldProxy } from 'sveltekit-superforms';
 
 	let {
@@ -68,63 +70,50 @@
 
 <Field {form} name={field}>
 	<Control let:attrs>
-		<Label class="flex justify-center flex-col sm:flex-row my-2">
-			<div class="flex flex-col gap-1">
-				<!-- svelte-ignore a11y-label-has-associated-control - $label contains the 'for' attribute -->
-				<label use:melt={$label}>
-					<span class="text-sm font-medium text-magnum-900">{$t(type + '.' + field)}</span>
-				</label>
+		<Label class="flex justify-center items-center flex-col sm:flex-row my-2 relative">
+			<span class="w-full max-w-xs">{$t(type + '.' + field)}</span>
+			<input use:melt={$input} class="input input-bordered w-full max-w-xs" {placeholder} />
 
-				<div class="relative">
-					<input
-						use:melt={$input}
-						class="input input-bordered flex h-10 items-center justify-between rounded-lg
-            px-3 pr-12"
-						{placeholder}
-					/>
-					<div class="absolute right-2 top-1/2 z-10 -translate-y-1/2 text-magnum-900">
-						<!-- {#if $open}
-				icono chevron-up
-			{:else}
-				icon chevron-down
-			{/if} -->
-					</div>
-				</div>
+			<div class="absolute right-2 top-1/2 z-10 -translate-y-1/2 text-magnum-900">
+				{#if $open}
+					<Icon src={AiOutlineUp} size="1.5em" />
+				{:else}
+					<Icon src={AiOutlineDown} size="1.5em" />
+				{/if}
 			</div>
-			<div id="combobox-menu"></div>
-			{#if $open}
-				<ul
-					class=" z-10 flex max-h-[300px] flex-col overflow-hidden rounded-lg"
-					use:melt={$menu}
-					transition:fly={{ duration: 150, y: -5 }}
+		</Label>
+		<div id="combobox-menu"></div>
+		{#if $open}
+			<ul
+				class=" z-10 flex max-h-[300px] flex-col overflow-hidden rounded-lg"
+				use:melt={$menu}
+				transition:fly={{ duration: 150, y: -5 }}
+			>
+				<div
+					class="flex max-h-full flex-col gap-0 overflow-y-auto bg-base-content px-2 py-2 text-black"
+					tabindex="0"
 				>
-					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-					<div
-						class="flex max-h-full flex-col gap-0 overflow-y-auto bg-base-content px-2 py-2 text-black"
-						tabindex="0"
-					>
-						{#each filteredInputArray as item, index (index)}
-							<li
-								use:melt={$option(toOption(item))}
-								class="relative cursor-pointer scroll-my-2 rounded-md py-2 pl-4 pr-4
+					{#each filteredInputArray as item, index (index)}
+						<li
+							use:melt={$option(toOption(item))}
+							class="relative cursor-pointer scroll-my-2 rounded-md py-2 pl-4 pr-4
           hover:bg-magnum-100
           data-[highlighted]:bg-magnum-200 data-[highlighted]:text-magnum-900
             data-[disabled]:opacity-50"
-							>
-								{#if $isSelected(item)}
-									<div class="check absolute left-2 top-1/2 z-10 text-magnum-900">icon check</div>
-								{/if}
-								<div class="pl-4">
-									<span class="font-medium">{item.label}</span>
-								</div>
-							</li>
-						{:else}
-							<li class="relative cursor-pointer rounded-md py-1 pl-8 pr-4">No results found</li>
-						{/each}
-					</div>
-				</ul>
-			{/if}
-		</Label>
+						>
+							{#if $isSelected(item)}
+								<div class="check absolute left-2 top-1/2 z-10 text-magnum-900">icon check</div>
+							{/if}
+							<div class="pl-4">
+								<span class="font-medium">{item.label}</span>
+							</div>
+						</li>
+					{:else}
+						<li class="relative cursor-pointer rounded-md py-1 pl-8 pr-4">No results found</li>
+					{/each}
+				</div>
+			</ul>
+		{/if}
 	</Control>
 	<FieldErrors />
 </Field>
