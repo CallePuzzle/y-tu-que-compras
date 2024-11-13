2241cd983e1bcb9a6421520acc7cfed5008cd50b
César M. Cristóbal <cesar@callepuzzle.com>
2024-11-13 16:44:34 +0100

    create api for update-quantity and update-unit

---
+++ src/routes/api/update-unit/+server.ts
@@ -0,0 +1,34 @@
+import { logger } from '$lib/server/logger';
+import { json, error } from '@sveltejs/kit';
+import { initializePrisma } from '$lib/server/db';
+
+import type { RequestEvent } from '@sveltejs/kit';
+
+export async function GET(event: RequestEvent): Promise<Response> {
+	const groceryListId = event.url.searchParams.get('id');
+	const unit = event.url.searchParams.get('unit');
+
+	logger.debug({ groceryListId, unit }, 'updateUnit');
+
+	const db = event.platform!.env.DB;
+	const prisma = initializePrisma(db);
+
+	try {
+		const groceryList = await prisma.groceryList.update({
+			where: {
+				id: parseInt(groceryListId as string)
+			},
+			data: {
+				unit: unit as string
+			},
+			include: {
+				grocery: true
+			}
+		});
+		logger.info('groceryList with id ' + groceryListId + ' updated');
+		return json({ groceryList });
+	} catch (e) {
+		logger.error(e);
+		error(500, e.message);
+	}
+}
