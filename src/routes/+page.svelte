e83b4161fc02e791c297e5fd877ddd4ad97b6504
César M. Cristóbal <cesar@callepuzzle.com>
2025-07-02 20:55:09 +0200

    fix styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="bg-base-100 border-base-300 collapse border">
+	<input type="radio" name="my-accordion-1" checked="checked" />
+	<div class="collapse-title font-semibold">How do I create an account?</div>
+	<div class="collapse-content text-sm">
+		Click the "Sign Up" button in the top right corner and follow the registration process.
+	</div>
+</div>
+<div class="bg-base-100 border-base-300 collapse border">
+	<input type="radio" name="my-accordion-1" />
+	<div class="collapse-title font-semibold">I forgot my password. What should I do?</div>
+	<div class="collapse-content text-sm">
+		Click on "Forgot Password" on the login page and follow the instructions sent to your email.
+	</div>
+</div>
+<div class="bg-base-100 border-base-300 collapse border">
+	<input type="radio" name="my-accordion-1" />
+	<div class="collapse-title font-semibold">How do I update my profile information?</div>
+	<div class="collapse-content text-sm">
+		Go to "My Account" settings and select "Edit Profile" to make changes.
+	</div>
+</div>
